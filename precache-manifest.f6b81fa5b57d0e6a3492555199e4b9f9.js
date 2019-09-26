8a312691f6e6f238f1e17724317e0adfb0f5a542
Ulyanova Anastasia <uljanova.anastasia@gmail.com>
2019-09-26 20:58:48 +0300

    Updates

---
+++ precache-manifest.f6b81fa5b57d0e6a3492555199e4b9f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b828aafa82b7f2e6abef04f5e0cbbea",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "513319754768132ed340",
+    "url": "/portfolio/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "fd758e5f99da7ad89eed",
+    "url": "/portfolio/static/js/2.aacca056.chunk.js"
+  },
+  {
+    "revision": "513319754768132ed340",
+    "url": "/portfolio/static/js/main.d4ff5e65.chunk.js"
+  },
+  {
+    "revision": "9821c9659a93692e486a",
+    "url": "/portfolio/static/js/runtime-main.14a56db7.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/portfolio/static/media/logo.25bf045c.svg"
+  }
+]);
